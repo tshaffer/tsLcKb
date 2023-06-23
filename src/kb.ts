5caac4521c84b2a47ea4275dea58086aaae9b132
Ted Shaffer <shaffer.family@gmail.com>
2023-06-23 05:58:19 -0700

    successful examples of using FAISS with langchain

--- src/kb.ts
+++ src/kb.ts
@@ -1,13 +1,34 @@
+// import { FaissStore } from "langchain/vectorstores/faiss";
+// import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+
+// export const run = async () => {
+//   const vectorStore = await FaissStore.fromTexts(
+//     ["Hello world", "Bye bye", "hello nice world"],
+//     [{ id: 2 }, { id: 1 }, { id: 3 }],
+//     new OpenAIEmbeddings()
+//   );
+
+//   const resultOne = await vectorStore.similaritySearch("hello world", 1);
+//   console.log(resultOne);
+// };
+
 import { FaissStore } from "langchain/vectorstores/faiss";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { TextLoader } from "langchain/document_loaders/fs/text";
 
 export const run = async () => {
-  const vectorStore = await FaissStore.fromTexts(
-    ["Hello world", "Bye bye", "hello nice world"],
-    [{ id: 2 }, { id: 1 }, { id: 3 }],
+  // Create docs with a loader
+  const loader = new TextLoader("/Users/tedshaffer/Documents/Projects/ai/tsLcKb/src/example.txt");
+  const docs = await loader.load();
+
+  // Load the docs into the vector store
+  const vectorStore = await FaissStore.fromDocuments(
+    docs,
     new OpenAIEmbeddings()
   );
 
+  // Search for the most similar document
   const resultOne = await vectorStore.similaritySearch("hello world", 1);
   console.log(resultOne);
 };
+
