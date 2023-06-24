371ee7787c0bb47e0c5a5a972b76e4b714f4ac78
Ted Shaffer <shaffer.family@gmail.com>
2023-06-24 08:20:49 -0700

    select specific column from csv.

--- src/kb.ts
+++ src/kb.ts
@@ -2,9 +2,17 @@ import { CSVLoader } from "langchain/document_loaders/fs/csv";
 
 export const run = async () => {
 
-  const loader = new CSVLoader("/Users/tedshaffer/Documents/Projects/ai/tsLcKb/src/example.csv");
+const loader = new CSVLoader(
+  "/Users/tedshaffer/Documents/Projects/ai/tsLcKb/src/example.csv",
+  "text"
+);
 
-  const docs = await loader.load();
-  console.log(docs);
+const docs = await loader.load();
+
+console.log(docs);
+  // const loader = new CSVLoader("/Users/tedshaffer/Documents/Projects/ai/tsLcKb/src/example.csv");
+
+  // const docs = await loader.load();
+  // console.log(docs);
 
 };
